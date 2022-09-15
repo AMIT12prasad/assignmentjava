4628db31c4aa17d3acab02de30fae95bb3eb02a9
AMIT12prasad <amitp8763@gmail.com>
2022-09-15 17:56:16 +0530

    assignmentjava

---
+++ Conditional.java
@@ -0,0 +1,38 @@
+package assignmentjava;
+
+public class Conditional {
+
+	public static void main(String[] args) {
+		//if statement
+		int x=18;
+		int y=20;
+		
+		if(x>y);
+		{System.out.println("x is greater than y");
+		}
+		
+		{
+			
+			//if else statement
+		    int a = 20;
+		    if (a < 24) {
+		      System.out.println("Good day.");
+		    } else {
+		      System.out.println("Good evening.");
+		    } 
+
+		    //else if statement
+		    int p = 22;
+		    if (p < 10) {
+		      System.out.println("Good morning.");
+		    } else if (p < 20) {
+		      System.out.println("Good day.");
+		    }  else {
+		      System.out.println("Good evening.");
+		    }
+		
+	
+	}
+
+	}
+	}
